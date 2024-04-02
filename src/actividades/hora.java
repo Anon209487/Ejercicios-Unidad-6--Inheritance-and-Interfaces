beee6707bdf33e529b31e2e891dfee12fb46e189
Anon209487 <156091669+Anon209487@users.noreply.github.com>
2024-04-02 14:38:50 +0200

    as

--- src/actividades/hora.java
+++ src/actividades/hora.java
@@ -6,13 +6,81 @@ package actividades;
 /** 
  * 
  */
-public class hora  {
+
+ class hora {
+	private int minutos;
+	private int horas;
+
+	
+
+	hora() {
+
+	}
+
+	hora(int minutos, int horas) {
+		this.minutos = minutos;
+		this.horas = horas;
+		this.segundos = segundos;
+	}
+
+	public void inc() {
+		this.minutos = this.minutos + 1;
+
+	}
+
+	public boolean setMinutosa(int minutos) {
+		boolean setMinutosa = false;
+		if (minutos >= 0) {
+			setMinutos(minutos);
+			setMinutosa = true;
+		}
+		return setMinutosa;
+
+	}
+
+	boolean setHora(int horas) {
+		boolean setHora = false;
+		if (horas >= 0 && horas < 23) {
+			setHoras(horas);
+			setHora = true;
+		}
+		return setHora;
+
+	}
+
+	public String toString() {
+		String hora = "La hora es  " + this.horas + ":" + this.minutos;
+		return hora;
+
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
 
 	/**
 	 * 
 	 */
-	public hora () {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 }
+
